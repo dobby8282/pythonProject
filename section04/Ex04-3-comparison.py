9310df8bc2c69956a04f2ae554765eb368c22cf0
ITSC <ITSC@example.com>
2023-06-03 13:24:33 +0900

    ex04

---
+++ section04/Ex04-3-comparison.py
@@ -0,0 +1,15 @@
+'''
+파일명: Ex04-3-comparison.py
+관계 연산자
+    2개 항을 비교하여 그 결과를 논리(bool) 자료형으로 반환
+    ex) >, >=, <, <=, ==, !=
+
+Ctrl + d : 줄복사
+'''
+a = 15
+print('{} > 10 : {}'.format(a, a > 10))
+print('{} < 10 : {}'.format(a, a < 10))
+print('{} >= 10 : {}'.format(a, a >= 10))
+print('{} <= 10 : {}'.format(a, a <= 10))
+print('{} == 10 : {}'.format(a, a == 10))
+print('{} != 10 : {}'.format(a, a != 10))
