91af1c4e117253f82a57a2c18795f69337cd27b2
ITSC <ITSC@example.com>
2023-06-18 12:48:41 +0900

    ex 18

--- section18/Ex18-2-beautifulsoup.py
+++ section18/Ex18-2-beautifulsoup.py
@@ -8,5 +8,19 @@ BeautifulSoup
 BeautifulSoup 설치방법
 pip install beautifulsoup4
 
+https://news.nate.com/rank?mid=n1000
 '''
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.nate.com/rank'
+param = {'mid': 'n1000'}
+response = requests.get(url, params=param)
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+tit_list = soup.find_all('h2')
+for tit in tit_list:
+    print(tit.text.strip())
 
