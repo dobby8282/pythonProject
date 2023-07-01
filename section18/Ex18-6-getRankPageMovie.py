dbe6e232693da4052da3c2e027328f6b9985e84e
ITSC <ITSC@example.com>
2023-07-01 10:46:10 +0900

    ex18

---
+++ section18/Ex18-6-getRankPageMovie.py
@@ -0,0 +1,18 @@
+'''
+파일명: Ex18-6-getRankPageMovie.py
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://movie.daum.net/ranking/boxoffice/weekly'
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+movie_list = soup.find_all('strong', class_='tit_item')
+
+for idx, movie in enumerate(movie_list):
+    print('{}위: {}'.format(idx+1, movie.text.strip()))
+
+
